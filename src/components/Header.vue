07bdd6db5b008de817e5d4237e0baa9254c6b891
muguisuci <3515851731@qq.com>
2023-12-11 09:48:53 +0800

    vue-3

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <div class="w-screen h-[10vw] bg-white dark:bg-[black] ">
         <div class="w-screen h-[8vw] flex justify-around items-center">
             <Drawer  class="dark:bg-[black]"></Drawer>
-            <div>dianjituichu</div>
+            <div @click="deleteCachedData">dianjituichu</div>
             <label class="relative">
                 <input type="text" placeholder="Love Is Gone (Acoustic)" class="text-[2vw] pl-[8vw] bg-gradient-to-r from-[#d9ddfd] text-[#8189A1] to-[#f3d9ef] w-[75vw] h-[6vw] border-2 border-[#CCCEF7] rounded-[25px]">
                 <span class="text-[4vw] absolute top-[1vw] left-[3vw]"><Icon icon="basil:search-solid" color="#666" /></span>
