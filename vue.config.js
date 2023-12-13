4bec51b0e5ef03bdb5f7b88533fa9f43a1397afa
muguisuci <3515851731@qq.com>
2023-12-13 17:00:47 +0800

    网易云测试

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,16 @@ module.exports = defineConfig({
   // 设置正向代理服务器
   devServer:{
     proxy:"http://111.229.247.11:3000/",
+  },
+  configureWebpack:{
+    devtool: "source-map",
+    plugins: [
+      sentryWebpackPlugin({
+        org: "HOPU",
+        project: "vue-search",
+        authToken:"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"
+      }),
+    ]
   }
+
 })
